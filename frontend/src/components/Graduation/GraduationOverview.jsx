ec800279a66e4e8d8da2c9afe4ace3109594f29a
shanelee2027 <shane.lee@yale.edu>
2023-11-11 17:55:12 -0500

    added graduation page

---
+++ frontend/src/components/Graduation/GraduationOverview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function GraduationOverview() {
+  return (
+    <Card style={{ width: "50rem" }}>
+      <Card.Title>Overview</Card.Title>
+      <Card.Body>This is some text within a card body.</Card.Body>
+    </Card>
+  );
+}
+
+export default GraduationOverview;
