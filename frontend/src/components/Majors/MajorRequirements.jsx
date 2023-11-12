895c30ca00ee79f2c35ff8ee2214299e22cf40f0
Jason Zheng <jason.zheng@yale.edu>
2023-11-12 13:34:49 -0500

    Fixed tab notation

--- frontend/src/components/Majors/MajorRequirements.jsx
+++ frontend/src/components/Majors/MajorRequirements.jsx
@@ -2,57 +2,112 @@ import React from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Card from 'react-bootstrap/Card';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Nav } from 'react-bootstrap';
 
 function MajorRequirements() {
     return (
+        // <Card>
+        //     <Card.Body>
+        //         <Card.Title>
+        //             <Row>
+        //                 <Col>
+        //                     <h1>Requirements</h1>
+        //                 </Col>
+        //                 <Col>
+        //                     <Tabs
+        //                         dafaultActiveKey="List"
+        //                         id="list-tab"
+        //                     >   
+        //                         <Tab eventKey="List" title="List">
+        //                             {/* Create a Course component and then connect to firebase and map through the data using the course component */}
+        //                             <h1>CORE</h1>
+        //                             <ul>
+        //                                 <li>CPSC 201</li>
+        //                                 <li>CPSC 202/ MATH 224</li>
+        //                                 <li>CPSC 223</li>
+        //                                 <li>CPSC 323</li>
+        //                                 <li>CPSC 365 / CPSC 366 / CPSC 368</li>
+        //                             </ul>
+        //                             <h1>Electives</h1>
+        //                             <p>The field of computer science has broadened substantially in the last few decades and the Computer Science department advises its majors to choose intermediate and advanced electives covering the breadth of computer science,
+        //                                 including theoretical computer science; computer systems and languages (e.g., database, networking, operating systems, programming languages, and systems security);
+        //                                 and computer applications (e.g., artificial intelligence, computer graphics, computer vision, human-computer interactions, machine learning, natural language processing, and robotics).</p>
+        //                             <h2>Likely</h2>
+        //                             <ul>
+        //                                 <li>LING 385</li>
+        //                                 <li>NSCI 258</li>
+        //                             </ul>
+        //                             <h2>Popular</h2>
+        //                             <ul>
+        //                                 <li>CPSC 370</li>
+        //                                 <li>CPSC 419</li>
+        //                             </ul>
+        //                             <h1>SENIOR REQUIREMENT</h1>
+        //                             <ul>
+        //                                 <li>CPSC 490</li>
+        //                             </ul>
+        //                         </Tab>
+        //                         <Tab eventKey="Graph" title="Graph">
+        //                             <h1>INSERT GRAPH VIEW HERE</h1>
+        //                         </Tab>
+        //                     </Tabs>
+        //                 </Col>
+        //             </Row>
+        //         </Card.Title>
+        //     </Card.Body>
+        // </Card>
         <Card>
             <Card.Body>
-                <Card.Title>
+                <Tab.Container id="tabs" defaultActiveKey="first">
                     <Row>
-                        <Col>
-                            <h1>Requirements</h1>
+                    <Col><h1>Requirements</h1></Col>
+                    <Col>
+                        <Nav variant="pills" className="nav-pills" id="pills-tab">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">List View</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Graph View</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
                         </Col>
-                        <Col>
-                            <Tabs
-                                dafaultActiveKey="List"
-                                id="list-tab"
-                            >   
-                                <Tab eventKey="List" title="List">
-                                    <h1>CORE</h1>
-                                    <ul>
-                                        <li>CPSC 201</li>
-                                        <li>CPSC 202/ MATH 224</li>
-                                        <li>CPSC 223</li>
-                                        <li>CPSC 323</li>
-                                        <li>CPSC 365 / CPSC 366 / CPSC 368</li>
-                                    </ul>
-                                    <h1>Electives</h1>
-                                    <p>The field of computer science has broadened substantially in the last few decades and the Computer Science department advises its majors to choose intermediate and advanced electives covering the breadth of computer science,
-                                        including theoretical computer science; computer systems and languages (e.g., database, networking, operating systems, programming languages, and systems security);
-                                        and computer applications (e.g., artificial intelligence, computer graphics, computer vision, human-computer interactions, machine learning, natural language processing, and robotics).</p>
-                                    <h2>Likely</h2>
-                                    <ul>
-                                        <li>LING 385</li>
-                                        <li>NSCI 258</li>
-                                    </ul>
-                                    <h2>Popular</h2>
-                                    <ul>
-                                        <li>CPSC 370</li>
-                                        <li>CPSC 419</li>
-                                    </ul>
-                                    <h1>SENIOR REQUIREMENT</h1>
-                                    <ul>
-                                        <li>CPSC 490</li>
-                                    </ul>
-                                </Tab>
-                                <Tab eventKey="Graph" title="Graph">
-                                    <h1>INSERT GRAPH VIEW HERE</h1>
-                                </Tab>
-                            </Tabs>
-                        </Col>
-                    </Row>
-                </Card.Title>
+                        </Row>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <div className="requirements-tab">
+                                <h1>CORE</h1>
+                                <ul>
+                                    <li>CPSC 201</li>
+                                    <li>CPSC 202/ MATH 224</li>
+                                    <li>CPSC 223</li>
+                                    <li>CPSC 323</li>
+                                    <li>CPSC 365 / CPSC 366 / CPSC 368</li>
+                                </ul>
+                                <h1>Electives</h1>
+                                <p>The field of computer science has broadened substantially in the last few decades and the Computer Science department advises its majors to choose intermediate and advanced electives covering the breadth of computer science,
+                                    including theoretical computer science; computer systems and languages (e.g., database, networking, operating systems, programming languages, and systems security);
+                                    and computer applications (e.g., artificial intelligence, computer graphics, computer vision, human-computer interactions, machine learning, natural language processing, and robotics).</p>
+                                <h2>Likely</h2>
+                                <ul>
+                                    <li>LING 385</li>
+                                    <li>NSCI 258</li>
+                                </ul>
+                                <h2>Popular</h2>
+                                <ul>
+                                    <li>CPSC 370</li>
+                                    <li>CPSC 419</li>
+                                </ul>
+                                <h1>SENIOR REQUIREMENT</h1>
+                                <ul>
+                                    <li>CPSC 490</li>
+                                </ul>
+                            </div>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                            <h1>INSERT GRAPH VIEW HERE</h1>
+                        </Tab.Pane>
+                    </Tab.Content>
+                </Tab.Container>
             </Card.Body>
         </Card>
     );
