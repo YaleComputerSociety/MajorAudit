bf13702034c7ea07bd8dccc403d2998bf2695de2
Bradley Lewis <bradleylewis@Bradleys-MBP-2.wireless.yale.internal>
2024-02-18 15:09:04 -0500

    More boilerplate

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,5 +1,3 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Courses from "./pages/Courses";
