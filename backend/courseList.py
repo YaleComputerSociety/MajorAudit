081fabaea53f9569a37e3b1363b03fb610647d03
Oz Gitelson <47493278+OzGitelson@users.noreply.github.com>
2024-02-18 20:20:56 -0500

    add get_remaining_requirements

--- backend/courseList.py
+++ backend/courseList.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import copy
 
 # Use a service account.
 # with open(r'C:\YCS\MajorAudit/cwd.txt', 'w') as outfile:
@@ -14,12 +15,15 @@ db = firestore.client()
 
 
 class CourseList:
-    def __init__(self, num_needed=float('inf')):
+    def __init__(self, num_needed=float('inf'), courses=None):
         self.num_needed=num_needed
-        self.courses=[]
+        if courses:
+            self.courses=courses
+        else:
+            self.courses=set()
 
     def add_course(self, course):
-        self.courses.append(course)
+        self.courses.add(course)
 
     def to_dict(self):
         ret_dic= {'num_needed': self.num_needed, 'courses': []}
@@ -45,7 +49,7 @@ class CourseList:
 
     def __str__(self):
         ret_list=[str(c) for c in self.courses]
-        req_str=str(self.num_needed) if self.num_needed<float('inf') else 'all'
+        req_str=str(self.num_needed) if self.num_needed<len(self.get_courses()) else 'all'
         ret_str=f'{req_str} of: ['
         for c in ret_list:
             ret_str+=f'{c}, '
@@ -58,18 +62,39 @@ class CourseList:
     def get_required(self):
         return min(self.num_needed, len(self.courses))
 
+    def remove_course(self, course):
+        removed=course in self.courses
+        self.courses.discard(course)
+        return removed
+
+
+def get_remaining_requirements(courses:CourseList, courses_taken):
+    remaining_courses=set()
+    needed=0
+    if not requirements_completed(courses, courses_taken):
+        for c in courses.get_courses():
+            if type(c) is str and c not in courses_taken:
+                remaining_courses.add(c)
+                needed+=1
+            elif type(c) is not str and not requirements_completed(c, courses_taken):
+                remaining_courses.add(get_remaining_requirements(c, courses_taken))
+                needed+=1
+
+    if needed==0:
+        return None
+    if needed==1:
+        return list(remaining_courses)[0]
+
+    new_list=CourseList(needed, remaining_courses)
+    print(new_list.get_required(), new_list)
+
+    return new_list
+
+
+
 
 def requirements_completed(courses: CourseList, courses_taken):
     num_completed=0
-    # courses_taken=set()
-    # courses_from_db=db.get()._data['courses']
-    # # print(courses_from_db._data)
-    # # return
-    # print(courses_from_db)
-    # for semester in courses_from_db:
-    #     courses_taken=courses_taken.union(semester)
-    #
-    # print(courses_taken)
 
     for c in courses.get_courses():
         if type(c) is str:
@@ -85,10 +110,10 @@ def requirements_completed(courses: CourseList, courses_taken):
 
 
 if __name__=='__main__':
-    courses=CourseList(1)
+    courses=CourseList()
 
     course_group = CourseList()
-    course_group.add_course('CPSC 223')
+    course_group.add_course('CPSC 423')
     course_group.add_course('CPSC 323')
     courses.add_course(course_group)
 
@@ -96,6 +121,8 @@ if __name__=='__main__':
     course_group.add_course('CPSC 470')
     course_group.add_course('CPSC 472')
     courses.add_course(course_group)
+
+    courses.add_course('ASL 130')
 
     course_dic=courses.to_dict()
     courses=CourseList.from_dict(course_dic)
@@ -109,6 +136,7 @@ if __name__=='__main__':
         courses_taken = courses_taken.union(courses_from_db[semester])
 
     print(requirements_completed(courses, courses_taken))
+    print(get_remaining_requirements(courses, courses_taken))
 
 
 
