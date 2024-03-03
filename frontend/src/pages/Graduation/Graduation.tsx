c898e853fef8f27127f442061ef18b34bdf2f162
winbow13 <benjaminwu13@gmail.com>
2024-03-03 15:07:21 -0500

    Button!
    Button to switch between years works!

--- frontend/src/pages/Graduation/Graduation.tsx
+++ frontend/src/pages/Graduation/Graduation.tsx
@@ -1,23 +1,29 @@
-import React from "react";
-import Table from "react-bootstrap/Table";
+import React, { useState } from "react";
 import DistributionTable from "./components/DistributionTable";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
 import styles from "./Graduation.module.css";
 
-var currYear = 2;
-
 export default function Graduation() {
+  const [currYear, setCurrYear] = React.useState("2");
   return (
     <div>
       <div>
         <p style={{ fontSize: "30px" }}>Distributions</p>
-        <button onClick={() => {currYear = 1}}>First-Year</button>
-        <button onClick={() => {currYear = 2}}>Sophomore</button>
-        <button onClick={() => {currYear = 3}}>Junior</button>
-        <button onClick={() => {currYear = 4}}>Senior</button>
+        <ToggleButtonGroup
+          color="primary"
+          onChange={(event, newYear) => {setCurrYear(newYear);}}
+          aria-label="Platform"
+        >
+          <ToggleButton value="1">First-Year</ToggleButton>
+          <ToggleButton value="2">Sophomore</ToggleButton>
+          <ToggleButton value="3">Junior</ToggleButton>
+          <ToggleButton value="4">Senior</ToggleButton>
+        </ToggleButtonGroup>
       </div>
       <div className={styles.distributionsContainer}>
         <DistributionTable
-          year={currYear}
+          year={parseInt(currYear)}
           classListHu={["LING 191", "ARCH 306"]}
           classListSo={[]}
           classListSc={["CGSC 274"]}
