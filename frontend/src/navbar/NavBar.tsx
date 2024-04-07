4861d21cd41dfba6850349778db0d6a0af1eae55
Lejun Deng <darwindenglejun@gmail.com>
2024-04-07 12:20:01 -0400

    Profile dropdown

--- frontend/src/navbar/NavBar.tsx
+++ frontend/src/navbar/NavBar.tsx
@@ -1,31 +1,57 @@
-
-import React from 'react';
+import React from "react";
 import styles from "./NavBar.module.css";
 
 import img_logo from "./../commons/images/ma_logo.png";
 import img_moon from "./../commons/images/moon.png";
-import img_profile from "./../commons/images/profile.png";
 
 import { NavLink } from "react-router-dom";
+import MeDropdown from "./MeDropdown";
 
 function NavBar() {
-    return (    
-        <div className={styles.NavBar}>
+  return (
+    <div className={styles.NavBar}>
+      <div className={styles.row} style={{ marginLeft: "20px" }}>
+        <img
+          src={img_logo}
+          alt=""
+          style={{ width: "150px", height: "auto", marginRight: "10px" }}
+        />
+      </div>
 
-            <div className={styles.row} style={{ marginLeft: "20px" }}>
-                <img src={img_logo} alt="" style={{ width: "150px", height: "auto", marginRight: "10px" }}/>
-            </div>
-            
-            <div className={styles.row} style={{ marginRight: "50px" }}>
-                <img src={img_moon} alt="" style={{ width: "40px", height: "30px", marginRight: "2px" }}/>
-                <NavLink to="/graduation"  className={({ isActive }) => isActive ? styles.activeLink : styles.dormantLink}>Graduation</NavLink>
-                <NavLink to="/courses"     className={({ isActive }) => isActive ? styles.activeLink : styles.dormantLink}>Courses</NavLink>
-                <NavLink to="/majors"      className={({ isActive }) => isActive ? styles.activeLink : styles.dormantLink}>Majors</NavLink>
-                <img src={img_profile} alt="" style={{ width: "24px", height: "24px", marginLeft: "14px" }}/>
-            </div>
-
-        </div>
-    )
+      <div className={styles.row} style={{ marginRight: "10px" }}>
+        <img
+          src={img_moon}
+          alt=""
+          style={{ width: "40px", height: "30px", marginRight: "2px" }}
+        />
+        <NavLink
+          to="/graduation"
+          className={({ isActive }) =>
+            isActive ? styles.activeLink : styles.dormantLink
+          }
+        >
+          Graduation
+        </NavLink>
+        <NavLink
+          to="/courses"
+          className={({ isActive }) =>
+            isActive ? styles.activeLink : styles.dormantLink
+          }
+        >
+          Courses
+        </NavLink>
+        <NavLink
+          to="/majors"
+          className={({ isActive }) =>
+            isActive ? styles.activeLink : styles.dormantLink
+          }
+        >
+          Majors
+        </NavLink>
+        <MeDropdown />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
