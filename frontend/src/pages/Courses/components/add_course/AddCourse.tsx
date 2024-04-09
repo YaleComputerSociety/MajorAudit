9d410121507239dda6fd5e5923640a11be273457
RyanGumlia <ryan.gumlia@yale.edu>
2024-04-09 18:03:02 -0400

    dinner

--- frontend/src/pages/Courses/components/add_course/AddCourse.tsx
+++ frontend/src/pages/Courses/components/add_course/AddCourse.tsx
@@ -2,10 +2,22 @@
 import React from "react";
 import styles from "./AddCourse.module.css";
 
+import CourseBoxSmall from "./../../../../commons/components/courses/CourseBoxSmall";
+
+import { MockCourses } from "../../../../commons/mock/MockCourses";
+
 function AddCourseMenu(){
     return(
-        <div>
+        <div className={styles.CourseMenuContainer}>
             <input type="text" placeholder="Search Courses..." className={styles.SearchBar}/>
+            <div>
+                <div className={styles.SectionHeader}>
+                    Pinned Courses
+                </div>
+                {MockCourses.map(course => (
+                    <CourseBoxSmall course={course} />
+                ))}
+            </div>
         </div>
     );
 }
