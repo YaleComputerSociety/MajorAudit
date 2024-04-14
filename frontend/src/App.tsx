b0f559024a0fab23a4ebead497a5f5c47356bbe6
RyanGumlia <ryan.gumlia@yale.edu>
2024-04-14 14:48:16 -0400

    fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import React from "react";
 
 import { Route, Routes } from 'react-router-dom';
 
-import Navbar from './navbar/NavBar';
 import Courses from './pages/Courses';
 import Graduation from './pages/Graduation';
 import Majors from './pages/Majors';
@@ -12,16 +11,11 @@ import Majors from './pages/Majors';
 function App() {
   return (
   <div>
-    <div style={{ position: "relative", zIndex: "2" }}>
-      <Navbar/>
-    </div>
-    <div style={{ position: "relative", zIndex: "1" }}>
       <Routes>
-        <Route path=""             element={<Graduation />} />      
-        <Route path="/courses"      element={<Courses/>} />
+        <Route path="/"              element={<Graduation/>}/>      
+        <Route path="/courses"      element={<Courses/>}/>
         <Route path="/majors"       element={<Majors/>}/>
       </Routes>
-    </div>
   </div>
   );
 }
--- frontend/src/pages/Majors/Majors.tsx
+++ frontend/src/pages/Majors/Majors.tsx
@@ -6,7 +6,35 @@ import ProgramRequirementsBox from "./components/ProgramRequirementsBox";
 import ProgramMetadataBox from "./components/ProgramMetadataBox";
 
 import { CPSC, ECON, HIST } from "./../../commons/mock/MockProgram";
+
+import nav_styles from "./../../commons/components/navbar/NavBar.module.css";
+import img_logo from "./../../commons/images/ma_logo.png";
+import { NavLink } from "react-router-dom";
+
 const programs = [CPSC, ECON, HIST];
+
+function NavBar() {
+  return (
+    <div className={nav_styles.NavBar}>
+      <div style={{ marginLeft: "20px" }}>
+        <img src={img_logo} alt="" style={{ width: "150px", height: "auto", marginRight: "10px" }}/>
+      </div>
+
+      <div className={nav_styles.row} style={{ marginRight: "20px" }}>
+        <NavLink to="/" className={({ isActive }) => isActive ? nav_styles.activeLink : nav_styles.dormantLink }>
+          Graduation
+        </NavLink>
+        <NavLink to="/courses" className={({ isActive }) => isActive ? nav_styles.activeLink : nav_styles.dormantLink }>
+          Courses
+        </NavLink>
+        <NavLink to="/majors" className={({ isActive }) => isActive ? nav_styles.activeLink : nav_styles.dormantLink }>
+          Majors
+        </NavLink>
+        {/* <MeDropdown /> */}
+      </div>
+    </div>
+  );
+}
 
 export const Majors = () => {
 
@@ -27,14 +55,17 @@ export const Majors = () => {
   };
   
   return(
-    <div className={styles.MajorsPage}>
-      <ProgramMetadataBox 
-        program={programs[currdex]} 
-        scrollProgram={alterCurrdex} 
-        seeProgram={seeProgram}
-        whichDegree={currDegree}
-        alterCurrDegree={alterCurrDegree}/>
-      <ProgramRequirementsBox degree={programs[currdex].degrees[currDegree]}/>
+    <div>
+      <NavBar/>
+      <div className={styles.MajorsPage}>
+        <ProgramMetadataBox 
+          program={programs[currdex]} 
+          scrollProgram={alterCurrdex} 
+          seeProgram={seeProgram}
+          whichDegree={currDegree}
+          alterCurrDegree={alterCurrDegree}/>
+        <ProgramRequirementsBox degree={programs[currdex].degrees[currDegree]}/>
+      </div>
     </div>
   );
 };
