4718d3c6208477bdc52ba490ee7778bce4a55cd5
winbow13 <benjaminwu13@gmail.com>
2024-04-20 18:53:33 -0400

    Reverted many changes
    Changed to only have clickable icons and a minor CSS change.

--- frontend/src/commons/components/courses/CourseBoxSmall.tsx
+++ frontend/src/commons/components/courses/CourseBoxSmall.tsx
@@ -1,42 +1,14 @@
 import React from "react";
-import { useState } from 'react'
 import styles from "./CourseBoxSmall.module.css";
 
 import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip } from "react-tooltip";
-import { Dialog } from '@headlessui/react'
-
 import { Season, StudentCourse, Course } from "../../types/TypeCourse";
 
 import img_fall from "./../../images/fall.png";
 import img_spring from "./../../images/spring.png";
 
 import DistributionCircle from "./DistributionsCircle";
-
-function CourseCheckmark(props: {enrollmentStatus: string}) {
-  if (props.enrollmentStatus === "COMPLETED") {
-    return (
-      <div
-      style={{
-        paddingLeft: "1px",
-        paddingRight: "3px",
-      }}
-    >
-      <div
-        data-tooltip-id="check-tooltip"
-        data-tooltip-content="Credit Confirmed by Yale"
-        data-tooltip-place="top"
-      >
-        ✓
-      </div>
-      <Tooltip id="check-tooltip" style={{ 
-      backgroundColor: "#444444",
-      borderRadius: "3px"}}/>
-    </div>
-    )
-  }
-  return <div></div>
-}
 
 function CourseSeasonIcon(props: { seasons: Array<Season> }) {
   const seasonImageMap = {
@@ -63,42 +35,13 @@ function CourseSeasonIcon(props: { seasons: Array<Season> }) {
 }
 
 function CourseBoxSmall(props: { studentCourse?: StudentCourse, course?: Course } ) {
-  let [isOpen, setIsOpen] = useState(false)
-
-  function CourseModal() {
-    return (
-      <Dialog className="Dialog" open={isOpen} onClose={closeModal}>
-        <Dialog.Panel>
-          <Dialog.Title>{props.course?.title}</Dialog.Title>
-          <Dialog.Description>
-            {props.course?.title}
-          </Dialog.Description>
-  
-          <p>
-          {props.course?.description}
-          </p>
-  
-          <button onClick={closeModal}>Close</button>
-        </Dialog.Panel>
-      </Dialog>
-    )
-  }
-
-  function closeModal() {
-    setIsOpen(false)
-  }
-
-  function openModal() {
-    setIsOpen(true)
-  }
-
   /* Depending on if you pass through a student course or a normal course,
    it will render a different coursebox, with a checkmark for studentCourse
    and without for a normal course */
   if (props.studentCourse == null && props.course != null) {
     return (
       <div>
-        <div className={styles.CourseBox} style={{ backgroundColor: "#F5F5F5" }} onClick={openModal}>
+        <div className={styles.CourseBox} style={{ backgroundColor: "#F5F5F5" }}>
           <CourseSeasonIcon seasons={props.course.seasons} />
           {props.course.code}
           {props.course.distribution.length > 0 ? (
@@ -109,26 +52,42 @@ function CourseBoxSmall(props: { studentCourse?: StudentCourse, course?: Course 
             ""
           )}
         </div>
-        <CourseModal />
       </div>
     );
   }
   else if (props.course == null && props.studentCourse != null) {
     return (
-      <div>
-      <div className={styles.CourseBox} style={{ backgroundColor: "#F5F5F5" }} onClick={openModal}>
-          <CourseCheckmark enrollmentStatus={props.studentCourse.enrollmentStatus}/>
-          <CourseSeasonIcon seasons={[props.studentCourse.season]} />
-          {props.studentCourse.course.code}
-          {props.studentCourse.course.distribution.length > 0 ? (
-            <div style={{paddingLeft: "2px"}}>
-            <DistributionCircle distributions={props.studentCourse.course.distribution}/>
+    <div className={styles.CourseBox} style={{ backgroundColor: "#F5F5F5" }}>
+        {props.studentCourse.enrollmentStatus === "COMPLETED" ? (
+          <div
+            style={{
+              paddingLeft: "1px",
+              paddingRight: "3px",
+            }}
+          >
+            <div
+              data-tooltip-id="check-tooltip"
+              data-tooltip-content="Credit Confirmed by Yale"
+              data-tooltip-place="top"
+            >
+              ✓
             </div>
-          ) : (
-            ""
-          )}
-        </div>
-        <CourseModal />
+            <Tooltip id="check-tooltip" style={{ 
+            backgroundColor: "#444444",
+            borderRadius: "3px"}}/>
+          </div>
+        ) : (
+          ""
+        )}
+        <CourseSeasonIcon seasons={[props.studentCourse.season]} />
+        {props.studentCourse.course.code}
+        {props.studentCourse.course.distribution.length > 0 ? (
+          <div style={{paddingLeft: "2px"}}>
+          <DistributionCircle distributions={props.studentCourse.course.distribution}/>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
       );
   }
--- frontend/src/commons/mock/MockCourses.ts
+++ frontend/src/commons/mock/MockCourses.ts
@@ -1,18 +1,18 @@
 
 import {Course, StudentCourse } from "./../types/TypeCourse";
 
-const CPSC201: Course = { code: "CPSC 201", title: "Title CPSC201", description: "Computer Science 201", seasons: ["FALL", "SPRING"],  evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
-const CPSC202: Course = { code: "CPSC 202", title: "Title CPSC202", description: "Computer Science 202", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
-const MATH244: Course = { code: "MATH 244", title: "Title MATH244", description: "Mathematics 244", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
-const CPSC223: Course = { code: "CPSC 223", title: "Title CPSC223", description: "Computer Science 223", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
-const CPSC323: Course = { code: "CPSC 323", title: "Title CPSC323", description: "Computer Science 323", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
-const CPSC365: Course = { code: "CPSC 365", title: "Title CPSC365", description: "Computer Science 365", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
+const CPSC201: Course = { code: "CPSC 201", title: "Title CPSC201", description: "CPSC 201 is ... ", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
+const CPSC202: Course = { code: "CPSC 202", title: "Title CPSC202", description: "CPSC 202 is ... ", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
+const MATH244: Course = { code: "MATH 244", title: "Title MATH244", description: "CPSC 244 is ... ", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
+const CPSC223: Course = { code: "CPSC 223", title: "Title CPSC223", description: "CPSC 223 is ... ", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
+const CPSC323: Course = { code: "CPSC 323", title: "Title CPSC323", description: "CPSC 323 is ... ", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
+const CPSC365: Course = { code: "CPSC 365", title: "Title CPSC365", description: "CPSC 365 is ... ", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR"] };
 
 const Student201: StudentCourse = { course: CPSC201, enrollmentStatus: "COMPLETED", season: "FALL", year: "2022-2023", letterGrade: "A-" };
 const Student223: StudentCourse = { course: CPSC223, enrollmentStatus: "COMPLETED", season: "SPRING", year: "2022-2023", letterGrade: "A" };
 const Student323: StudentCourse = { course: CPSC323, enrollmentStatus: "ENROLLED", season: "FALL", year: "2022-2023", letterGrade: "" };
 
-const CGSC274: Course = { code: "CGSC 274", title: "Title CGSC274", description: "Cognitive Science 274", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR", "Sc", "So"] };
+const CGSC274: Course = { code: "CGSC 274", title: "Title CGSC274", description: "CGSC 274 is ... ", seasons: ["FALL", "SPRING"], evaluation: { rating: 4.1, workload: 4.2, professor: 5}, distribution: ["QR", "Sc", "So"] };
 const Student274: StudentCourse = { course: CGSC274, enrollmentStatus: "COMPLETED", season: "FALL", year: "2023-2024", letterGrade: "B" };
 
 export const MockCourses = [
