08f2c4645ad20f05324aa3bc0e8d96d7fdce3526
Oz Gitelson <47493278+OzGitelson@users.noreply.github.com>
2024-04-25 22:47:02 -0400

    login maybe working

--- backend/main.py
+++ backend/main.py
@@ -66,15 +66,27 @@ def login():
     # print(request.cookies, flush=True)
     # netid = session['NETID']
     # print("netid", netid)
+    service=get_redirect_url()
+    cookies={}
+
+
     if 'NETID' in session:
         print('sanity', file=sys.stderr)
-        return redirect('/majoraudit/us-central1/functions/dashboard')
+        if '127.0.0.1' in service:
+            # redirect_url='http://127.0.0.1:5000/dashboard'
+            redirect_url = 'http://127.0.0.1:3000'
+        else:
+            redirect_url = 'https://majoraudit.web.app'
 
-    service=get_redirect_url()
+        current_app.logger.info(f'redirecting to {redirect_url}')
+        resp = make_response(redirect(redirect_url))
+        for c in cookies:
+            resp.set_cookie(c, cookies[c])
+        return resp
+
     login_url=f'''https://secure.its.yale.edu/cas/login?service={service}'''
 
     redirect_url=login_url
-    cookies={}
 
     if 'ticket' in request.args:
         session['CAS_TOKEN'] = request.args['ticket']
@@ -85,6 +97,9 @@ def login():
         validation=validate(session['CAS_TOKEN'], service)
         if validation[0]:
             print("username", validation[1])
+            cookies['hello']='world'
+            if 'NETID' in session:
+                cookies['wtf']=session['NETID']
             print(session)
             
             user = User(validation[1], "")
@@ -95,9 +110,9 @@ def login():
 
             if '127.0.0.1' in service:
                 #redirect_url='http://127.0.0.1:5000/dashboard'
-                redirect_url = 'http://127.0.0.1:5000'
+                redirect_url = 'http://127.0.0.1:3000'
             else:
-                redirect_url='https://majoraudit.web.app/dashboard'
+                redirect_url='https://majoraudit.web.app'
 
             # response = make_response(redirect('/majoraudit/us-central1/functions/dashboard'))
             # expires = datetime.datetime.now() + datetime.timedelta(days=30)
