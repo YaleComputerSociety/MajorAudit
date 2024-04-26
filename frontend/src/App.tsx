3e67658c2276a58081fee1c23965bb7a99d0f8de
winbow13 <benjaminwu13@gmail.com>
2024-04-25 22:33:42 -0400

    Fixed dev to be up to date with login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,39 @@
+
 import { useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes  } from 'react-router-dom'; 
 
 import Login from "./pages/Login";
-import Graduation from "./pages/Graduation";
-import Courses from "./pages/Courses";
-import Majors from "./pages/Majors";
-import FAQ from "./pages/OtherPages/FAQ/FAQ";
-import About from "./pages/OtherPages/About/About";
+import Graduation from './pages/Graduation';
+import Courses from './pages/Courses';
+import Majors from './pages/Majors';
 
 import { CGSC, CPSC, ECON, HIST } from "./commons/mock/MockProgram";
 
 function App() {
-  const [auth] = useState(true);
+
+  const [auth] = useState(true); 
 
   const programs = [CGSC, CPSC, ECON, HIST];
   const strPrograms = JSON.stringify(programs);
   localStorage.setItem("programList", strPrograms);
-
+  
   return (
-    <div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            auth ? <Navigate to="/graduation" /> : <Navigate to="/login" />
-          }
-        />
-        <Route
-          path="/login"
-          element={auth ? <Navigate to="/graduation" /> : <Login />}
-        />
+  <div>
+    <Routes>
+      {/* <Route path="/"             element={auth ? <Navigate to="/graduation"/> : <Navigate to="/login"/>}/> 
+      <Route path="/login"        element={auth ? <Navigate to="/graduation"/> : <Login/>}/> 
 
-        <Route
-          path="/graduation"
-          element={auth ? <Graduation /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/courses"
-          element={auth ? <Courses /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/majors"
-          element={auth ? <Majors /> : <Navigate to="/login" />}
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/FAQ" element={<FAQ />} />
-      </Routes>
-    </div>
+      <Route path="/graduation"   element={auth ? <Graduation/> : <Navigate to="/login"/>}/> 
+      <Route path="/courses"      element={auth ? <Courses/> : <Navigate to="/login"/>}/> 
+      <Route path="/majors"       element={auth ? <Majors/> : <Navigate to="/login"/>}/>  */}
+      <Route path="/"             element={<Navigate to="/login"/>}/> 
+      <Route path="/login"        element={<Login />}/> 
+
+      <Route path="/graduation"   element={<Graduation/>}/> 
+      <Route path="/courses"      element={<Courses/>}/> 
+      <Route path="/majors"       element={<Majors/>}/>
+    </Routes>
+  </div>
   );
 }
 
