c149e8839db1a736d0b87ec9b13f2a40cc346a2d
winbow13 <benjaminwu13@gmail.com>
2024-04-25 23:49:31 -0400

    Fixed CSS issues
    Still remains an issue when opening a class from the Courses Page: everything gets slightly shifted over and I don't know why

--- frontend/src/commons/components/courses/CourseModal.tsx
+++ frontend/src/commons/components/courses/CourseModal.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./CourseModal.module.css";
 import { useModal } from "../../../hooks/modalContext";
 import { Dialog } from "@headlessui/react";
 import { EMPTYCOURSE } from "../../mock/MockCourses";
@@ -13,57 +14,15 @@ export default function CourseModal() {
   return (
     <>
       <Dialog open={isOpen !== EMPTYCOURSE} onClose={closeModal} style={{ position: "relative", zIndex: "10" }}>
-        <div
-          style={{
-            position: "fixed",
-            inset: "0",
-            backgroundColor: "rgb(0, 0, 0, 0.25)",
-          }}
-        />
+        <div className={styles.modalBackground}/>
         <div style={{ position: "fixed", inset: "0", overflowY: "auto" }}>
-          <div
-            style={{
-              display: "flex",
-              minHeight: "100%",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: "4px",
-              textAlign: "center",
-            }}
-          >
-            <Dialog.Panel
-              style={{
-                width: "100%",
-                maxWidth: "28rem",
-                overflow: "hidden",
-                borderRadius: "1rem",
-                backgroundColor: "rgb(256, 256, 256)",
-                padding: "6px",
-                textAlign: "left",
-                alignItems: "middle",
-                boxShadow:
-                  "box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
-              }}
-            >
-              <Dialog.Title
-                as="h3"
-                style={{
-                  fontSize: "1.125rem",
-                  lineHeight: "1.5rem",
-                  fontWeight: "500",
-                  color: "rgb(17, 24, 39)",
-                }}
-              >
+          <div className={styles.panelPlacement}>
+            <Dialog.Panel className={styles.panelStyle}>
+              <Dialog.Title as="h3" className={styles.titleStyle}>
                 {isOpen !== undefined ? isOpen.title : 'No title'}
               </Dialog.Title>
               <div style={{ marginTop: "2px" }}>
-                <p
-                  style={{
-                    fontSize: "0.875rem",
-                    lineHeight: "1.25rem",
-                    color: "rgb(107, 114, 128)",
-                  }}
-                >
+                <p className={styles.descriptionStyle}>
                   {isOpen !== undefined ? isOpen.description : 'No description'}
                 </p>
               </div>
