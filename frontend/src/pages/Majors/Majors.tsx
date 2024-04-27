7b0829f48e7aad2c231e3802a21ad371eb0104ac
ramyareddy04 <75100092+ramyareddy04@users.noreply.github.com>
2024-04-27 13:37:29 -0400

    add pinned
    tbd links to program page when program is clicked

--- frontend/src/pages/Majors/Majors.tsx
+++ frontend/src/pages/Majors/Majors.tsx
@@ -4,60 +4,10 @@ import styles from "./Majors.module.css";
 import ProgramRequirementsBox from "./components/ProgramRequirementsBox";
 import ProgramMetadataBox from "./components/ProgramMetadataBox";
 
-import nav_styles from "./NavBar.module.css"
-import img_logo from "./../../commons/images/ma_logo.png";
-
 import { Program } from "./../../commons/types/TypeProgram";
-import MeDropdown from "../../navbar/account/MeDropdown";
-import { NavLink } from "react-router-dom";
-import SearchBar from "./components/SearchBar";
+import NavBar from "./components/NavBar"
 
-function NavBar() {
-  return (
-    <div className={nav_styles.NavBar}>
-      <div style={{ marginLeft: "20px" }}>
-        <img
-          src={img_logo}
-          alt=""
-          style={{ width: "150px", height: "auto", marginRight: "10px" }}
-        />
-      </div>
-      <div className={nav_styles.column}>
-        <SearchBar/>
-        <div className ={nav_styles.row}>
-          <text className={nav_styles.subheading}>PINNED</text>
-        </div>
-      </div>
-      <div className={nav_styles.row} style={{ marginRight: "20px" }}>
-        <NavLink
-          to="/graduation"
-          className={({ isActive }) =>
-            isActive ? nav_styles.activeLink : nav_styles.dormantLink
-          }
-        >
-          Graduation
-        </NavLink>
-        <NavLink
-          to="/courses"
-          className={({ isActive }) =>
-            isActive ? nav_styles.activeLink : nav_styles.dormantLink
-          }
-        >
-          Courses
-        </NavLink>
-        <NavLink
-          to="/majors"
-          className={({ isActive }) =>
-            isActive ? nav_styles.activeLink : nav_styles.dormantLink
-          }
-        >
-          Majors
-        </NavLink>
-        <MeDropdown />
-      </div>
-    </div>
-  );
-}
+
 function Majors() {
   const [currdex, setCurrdex] = useState(0);
   const [currDegree, setCurrDegree] = useState(0);
