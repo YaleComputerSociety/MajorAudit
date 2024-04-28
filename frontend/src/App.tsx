302c3b36e37499e251936db4b3c0fb46b75cca4f
lorenss-m <saeclmusic@gmail.com>
2024-04-28 14:19:34 -0400

    normaler version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,93 +8,44 @@ import Graduation from './pages/Graduation';
 import Courses from './pages/Courses';
 import Majors from './pages/Majors';
 
-interface AuthProps {
-  auth: boolean | null;
-}
+function App() {
 
-const withAuth = <P extends object>(WrappedComponent: React.ComponentType<P & AuthProps>) => {
-  return (props: P) => {
-    const [auth, setAuth] = useState(true);
+  const [auth, setAuth] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
     $.ajax({
       url: "http://127.0.0.1:5001/majoraudit/us-central1/functions/check_login",
       xhrFields: { withCredentials: true }
     }).done((data: string | null) => {
-      if (data) {
+      if(data) {
         console.log(data);
         setAuth(true);
-      } else {
+      }else{
         console.log(data);
         setAuth(false);
       }
+      setLoading(false);
     });
+  }, []);
 
-    if (auth === null) {
-      // Display a loading state while checking login
-      return <div>Loading...</div>;
-    }
+  if (loading) {
+    // Display a loading state while checking login
+    return <div>Loading...</div>;
+  }
 
-    return <WrappedComponent {...props} auth={auth} />;
-  };
-};
-
-interface AppProps extends AuthProps {}
-
-function App({ auth }: AppProps) {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={auth ? <Navigate to="/graduation" /> : <Navigate to="/login" />} />
-        <Route path="/login" element={auth ? <Navigate to="/graduation" /> : <Login />} />
-        <Route path="/graduation" element={auth ? <Graduation /> : <Navigate to="/login" />} />
-        <Route path="/courses" element={auth ? <Courses /> : <Navigate to="/login" />} />
-        <Route path="/majors" element={auth ? <Majors /> : <Navigate to="/login" />} />
-      </Routes>
-    </div>
+  <div>
+    <Routes>
+      <Route path="/"             element={auth ? <Navigate to="/graduation"/> : <Navigate to="/login"/>}/> 
+      <Route path="/login"        element={auth ? <Navigate to="/graduation"/> : <Login/>}/> 
+
+      <Route path="/graduation"   element={auth ? <Graduation/> : <Navigate to="/login"/>}/>
+      <Route path="/courses"      element={auth ? <Courses/> : <Navigate to="/login"/>}/> 
+      <Route path="/majors"       element={auth ? <Majors/> : <Navigate to="/login"/>}/> 
+    </Routes>
+  </div>
   );
 }
 
-export default withAuth(App);
-
-
-// function App() {
-
-//   const [auth, setAuth] = useState(true); 
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     $.ajax({
-//       url: "http://127.0.0.1:5001/majoraudit/us-central1/functions/check_login",
-//       xhrFields: { withCredentials: true }
-//     }).done((data: string | null) => {
-//       if(data) {
-//         console.log(data);
-//         setAuth(true);
-//       }else{
-//         console.log(data);
-//         setAuth(false);
-//       }
-//       setLoading(false);
-//     });
-//   }, []);
-
-//   if (loading) {
-//     // Display a loading state while checking login
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//   <div>
-//     <Routes>
-//       <Route path="/"             element={auth ? <Navigate to="/graduation"/> : <Navigate to="/login"/>}/> 
-//       <Route path="/login"        element={auth ? <Navigate to="/graduation"/> : <Login/>}/> 
-
-//       <Route path="/graduation"   element={auth ? <Graduation/> : <Navigate to="/login"/>}/>
-//       <Route path="/courses"      element={auth ? <Courses/> : <Navigate to="/login"/>}/> 
-//       <Route path="/majors"       element={auth ? <Majors/> : <Navigate to="/login"/>}/> 
-//     </Routes>
-//   </div>
-//   );
-// }
-
-// export default App;
+export default App;
