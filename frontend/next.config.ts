49495eee737f803eb5dec7722f63d58da19b4b49
RyanGumlia <ryan.gumlia@yale.edu>
2025-03-22 00:40:41 -0600

    css config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  reactStrictMode: true,
+  cssModules: true,
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 };
 
 export default nextConfig;
